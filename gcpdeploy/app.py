d86448df3793ebf16b5e18420cf3fb82d55d781c
Vignesh Sankar <90591505+VigneshSankar01@users.noreply.github.com>
2024-11-21 16:35:54 -0500

    Update app.py with health check

--- gcpdeploy/app.py
+++ gcpdeploy/app.py
@@ -8,6 +8,11 @@ def hello_world():
     """Simple Hello World route"""
     return "Hello World"
 
+@app.route("/health")
+def health_check():
+    """Health check route"""
+    return "ok", 200
+
 if __name__ == "__main__":
     # Run the app on port 8000
     app.run(host="0.0.0.0", port=8000)
